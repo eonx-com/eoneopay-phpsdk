428d7c39519d21d8b389ce764c3d74ad006f2c50
Julian Li <weiyi.li713@gmail.com>
2018-07-09 15:41:19 +1000

    rename interface

--- src/Requests/AbstractRequest.php
+++ src/Requests/AbstractRequest.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 namespace EoneoPay\PhpSdk\Requests;
 
 use LoyaltyCorp\SdkBlueprint\Sdk\BaseDataTransferObject;
-use LoyaltyCorp\SdkBlueprint\Sdk\Interfaces\RequestMethodInterface;
+use LoyaltyCorp\SdkBlueprint\Sdk\Interfaces\RequestMethodAwareInterface;
 use LoyaltyCorp\SdkBlueprint\Sdk\Interfaces\RequestObjectInterface;
 
-abstract class AbstractRequest extends BaseDataTransferObject implements RequestMethodInterface, RequestObjectInterface
+abstract class AbstractRequest extends BaseDataTransferObject implements RequestMethodAwareInterface, RequestObjectInterface
 {
     /**
      * {@inheritdoc}
