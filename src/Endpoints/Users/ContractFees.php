32582a07f963fd2fe2421c55deb6a1a08b907af9
Rashmit Singh <rashmitpal.singh@gmail.com>
2019-02-26 08:59:04 +1100

    adding security endpoints

---
+++ src/Endpoints/Users/ContractFees.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Codeint
+ * Date: 25/02/2019
+ * Time: 16:46
+ */
+
+namespace EoneoPay\PhpSdk\Endpoints\Users;
+
+
+class ContractFees
+{
+
+}
---
+++ src/Traits/Users/ContractFees.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Codeint
+ * Date: 25/02/2019
+ * Time: 16:47
+ */
+
+namespace EoneoPay\PhpSdk\Traits\Users;
+
+
+class ContractFees
+{
+
+}
