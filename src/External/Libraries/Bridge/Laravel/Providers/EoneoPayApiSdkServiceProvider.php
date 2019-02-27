d64f50595eba4a9486704f67d942dc5fd2ed7e56
Ketan Padia <ketan@loyaltycorp.com.au>
2019-02-28 10:04:59 +1100

    fix payment source repository, bind EoneoPayApiManager service in provider

--- src/External/Libraries/Bridge/Laravel/Providers/EoneoPayApiSdkServiceProvider.php
+++ src/External/Libraries/Bridge/Laravel/Providers/EoneoPayApiSdkServiceProvider.php
@@ -3,15 +3,12 @@ declare(strict_types=1);
 
 namespace EoneoPay\PhpSdk\External\Libraries\Bridge\Laravel\Providers;
 
+use EoneoPay\PhpSdk\External\Libraries\Bridge\Laravel\Providers\Sdk\SdkManagerServiceProvider;
+use EoneoPay\PhpSdk\Factories\ExceptionFactory;
+use EoneoPay\PhpSdk\Interfaces\EoneoPayApiManagerInterface;
+use EoneoPay\PhpSdk\Interfaces\Factories\ExceptionFactoryInterface;
+use EoneoPay\PhpSdk\Managers\EoneoPayApiManager;
 use Illuminate\Support\ServiceProvider;
-use Laravel\Lumen\Application;
-use LoyaltyCorp\SdkBlueprint\Sdk\Factories\SerializerFactory;
-use LoyaltyCorp\SdkBlueprint\Sdk\Factories\UrnFactory;
-use LoyaltyCorp\SdkBlueprint\Sdk\Handlers\RequestHandler;
-use LoyaltyCorp\SdkBlueprint\Sdk\Handlers\ResponseHandler;
-use LoyaltyCorp\SdkBlueprint\Sdk\Interfaces\Handlers\RequestHandlerInterface;
-use LoyaltyCorp\SdkBlueprint\Sdk\Interfaces\SdkManagerInterface;
-use LoyaltyCorp\SdkBlueprint\Sdk\Managers\SdkManager;
 
 class EoneoPayApiSdkServiceProvider extends ServiceProvider
 {
@@ -22,17 +19,11 @@ class EoneoPayApiSdkServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        // bind handlers
-        $this->app->singleton(RequestHandlerInterface::class, function (Application $app) {
-            return new RequestHandler(
-                $app->make('eoneopay_api_client'),
-                new ResponseHandler(),
-                new SerializerFactory(),
-                new UrnFactory()
-            );
-        });
+        // register sdk manager service provider
+        $this->app->register(SdkManagerServiceProvider::class);
 
-        // bind manager
-        $this->app->singleton(SdkManagerInterface::class, SdkManager::class);
+        // register EoneoPay api manager service
+        $this->app->singleton(ExceptionFactoryInterface::class, ExceptionFactory::class);
+        $this->app->singleton(EoneoPayApiManagerInterface::class, EoneoPayApiManager::class);
     }
 }
--- tests/External/Libraries/Bridge/Laravel/Providers/EoneoPayApiSdkServiceProviderTest.php
+++ tests/External/Libraries/Bridge/Laravel/Providers/EoneoPayApiSdkServiceProviderTest.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 namespace Tests\EoneoPay\PhpSdk\External\Libraries\Bridge\Laravel\Providers;
 
 use EoneoPay\PhpSdk\External\Libraries\Bridge\Laravel\Providers\EoneoPayApiSdkServiceProvider;
+use EoneoPay\PhpSdk\Factories\ExceptionFactory;
+use EoneoPay\PhpSdk\Interfaces\EoneoPayApiManagerInterface;
+use EoneoPay\PhpSdk\Interfaces\Factories\ExceptionFactoryInterface;
+use EoneoPay\PhpSdk\Managers\EoneoPayApiManager;
 use GuzzleHttp\Client;
 use Laravel\Lumen\Application;
 use LoyaltyCorp\SdkBlueprint\Sdk\Handlers\RequestHandler;
@@ -13,6 +17,7 @@ use LoyaltyCorp\SdkBlueprint\Sdk\Managers\SdkManager;
 use Tests\EoneoPay\PhpSdk\TestCase;
 
 /**
+ * @covers \EoneoPay\PhpSdk\External\Libraries\Bridge\Laravel\Providers\Sdk\SdkManagerServiceProvider
  * @covers \EoneoPay\PhpSdk\External\Libraries\Bridge\Laravel\Providers\EoneoPayApiSdkServiceProvider
  */
 class EoneoPayApiSdkServiceProviderTest extends TestCase
@@ -30,10 +35,10 @@ class EoneoPayApiSdkServiceProviderTest extends TestCase
         parent::setUp();
 
         $this->app = new Application();
-        $this->app->register(EoneoPayApiSdkServiceProvider::class);
         $this->app->singleton('eoneopay_api_client', function () {
             return new Client();
         });
+        $this->app->register(EoneoPayApiSdkServiceProvider::class);
     }
 
     /**
@@ -50,6 +55,14 @@ class EoneoPayApiSdkServiceProviderTest extends TestCase
         self::assertInstanceOf(
             SdkManager::class,
             $this->app->make(SdkManagerInterface::class)
+        );
+        self::assertInstanceOf(
+            ExceptionFactory::class,
+            $this->app->make(ExceptionFactoryInterface::class)
+        );
+        self::assertInstanceOf(
+            EoneoPayApiManager::class,
+            $this->app->make(EoneoPayApiManagerInterface::class)
         );
     }
 }
