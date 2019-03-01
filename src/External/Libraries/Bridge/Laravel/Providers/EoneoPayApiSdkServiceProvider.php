c2c0bb6a105e33b4835015f3de6c3378c3f149a8
Ketan Padia <37789589+ketanp77@users.noreply.github.com>
2019-03-01 11:06:20 +1100

    PR-31 review changes (#32)

--- src/External/Libraries/Bridge/Laravel/Providers/EoneoPayApiSdkServiceProvider.php
+++ src/External/Libraries/Bridge/Laravel/Providers/EoneoPayApiSdkServiceProvider.php
@@ -3,12 +3,17 @@ declare(strict_types=1);
 
 namespace EoneoPay\PhpSdk\External\Libraries\Bridge\Laravel\Providers;
 
-use EoneoPay\PhpSdk\External\Libraries\Bridge\Laravel\Providers\Sdk\SdkManagerServiceProvider;
 use EoneoPay\PhpSdk\Factories\ExceptionFactory;
 use EoneoPay\PhpSdk\Interfaces\EoneoPayApiManagerInterface;
-use EoneoPay\PhpSdk\Interfaces\Factories\ExceptionFactoryInterface;
 use EoneoPay\PhpSdk\Managers\EoneoPayApiManager;
+use GuzzleHttp\Client;
+use Illuminate\Contracts\Container\Container;
 use Illuminate\Support\ServiceProvider;
+use LoyaltyCorp\SdkBlueprint\Sdk\Factories\SerializerFactory;
+use LoyaltyCorp\SdkBlueprint\Sdk\Factories\UrnFactory;
+use LoyaltyCorp\SdkBlueprint\Sdk\Handlers\RequestHandler;
+use LoyaltyCorp\SdkBlueprint\Sdk\Handlers\ResponseHandler;
+use LoyaltyCorp\SdkBlueprint\Sdk\Managers\SdkManager;
 
 class EoneoPayApiSdkServiceProvider extends ServiceProvider
 {
@@ -19,11 +24,22 @@ class EoneoPayApiSdkServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        // register sdk manager service provider
-        $this->app->register(SdkManagerServiceProvider::class);
+        $this->app->singleton('eoneopay_api_client', function () {
+            return new Client(['base_uri' => \env('EONEOPAY_API_BASE_URI')]);
+        });
 
-        // register EoneoPay api manager service
-        $this->app->singleton(ExceptionFactoryInterface::class, ExceptionFactory::class);
-        $this->app->singleton(EoneoPayApiManagerInterface::class, EoneoPayApiManager::class);
+        $this->app->singleton(EoneoPayApiManagerInterface::class, function (Container $application) {
+            return new EoneoPayApiManager(
+                new SdkManager(
+                    new RequestHandler(
+                        $application->make('eoneopay_api_client'),
+                        new ResponseHandler(),
+                        new SerializerFactory(),
+                        new UrnFactory()
+                    )
+                ),
+                new ExceptionFactory()
+            );
+        });
     }
 }
