4f1061c42c72e2bb45d97d005b22d2cc9c98f01e
Brendon Tyree <brendon@tyree.co.nz>
2019-04-15 09:13:05 +1000

    PYMT-581: Implementation of CRN generation (#36)
    * PYMT-581: Implementation of ReferenceNumber for BPAY CRN generation
    
    * PYMT-581: Minor fix for standards
    
    * PYMT-581: Added ReferenceNumber to entity and adjusted test
    
    * Added test to check generated URIs, use of UserId field
    
    * PYMT-581: Further additions to User relationship, some minor refactoring
    
    * PYMT-581: Added missing completedAt column
    
    * PYMT-581: Code review change

---
+++ src/Endpoints/Users/ReferenceNumber.php
@@ -0,0 +1,46 @@
+<?php
+declare(strict_types=1);
+
+namespace EoneoPay\PhpSdk\Endpoints\Users;
+
+use EoneoPay\PhpSdk\Endpoints\User;
+use EoneoPay\PhpSdk\Traits\Users\ReferenceNumberTrait;
+use LoyaltyCorp\SdkBlueprint\Sdk\Entity;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+/**
+ * @method string getReferenceNumber()
+ * @method string getType()
+ * @method User getUser()
+ * @method setReferenceNumber(string $reference)
+ * @method setType(string $type)
+ * @method setUser(User $user)
+ */
+class ReferenceNumber extends Entity
+{
+    use ReferenceNumberTrait;
+
+    /**
+     * @Groups({"create", "get", "list", "update"})
+     *
+     * @var \EoneoPay\PhpSdk\Endpoints\Ewallet
+     */
+    protected $ewallet;
+
+    /**
+     * @Groups({"create", "get", "list", "update"})
+     *
+     * @var \EoneoPay\PhpSdk\Endpoints\User
+     */
+    protected $user;
+
+    /**
+     * @inheritdoc
+     */
+    public function uris(): array
+    {
+        return [
+            self::CREATE => '/reference'
+        ];
+    }
+}
