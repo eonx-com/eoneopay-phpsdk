5a3480dfa0037344be0f884d059767b73954c02d
Ketan Padia <ketan@loyaltycorp.com.au>
2019-09-20 15:47:10 +1000

    PYMT-1308 Fix Bpay payment source endpoint and test.

--- src/Endpoints/PaymentSources/Bpay.php
+++ src/Endpoints/PaymentSources/Bpay.php
@@ -4,41 +4,16 @@ declare(strict_types=1);
 namespace EoneoPay\PhpSdk\Endpoints\PaymentSources;
 
 use EoneoPay\PhpSdk\Endpoints\PaymentSource;
-use EoneoPay\PhpSdk\Endpoints\User;
 use EoneoPay\PhpSdk\Traits\PaymentSources\BpayTrait;
 
 /**
- * @method Ewallet|null getAllocationEwallet()
  * @method string|null getBillerCode()
  * @method string|null getBillerName()
- * @method Ewallet getEwallet()
  * @method string|null getReferenceNumber()
- * @method User|null getUser()
  */
 class Bpay extends PaymentSource
 {
     use BpayTrait;
-
-    /**
-     * Allocation ewallet associated with this Bpay.
-     *
-     * @var \EoneoPay\PhpSdk\Endpoints\PaymentSources\Ewallet|null
-     */
-    protected $allocationEwallet;
-
-    /**
-     * Ewallet associated with this Bpay.
-     *
-     * @var \EoneoPay\PhpSdk\Endpoints\PaymentSources\Ewallet
-     */
-    protected $ewallet;
-
-    /**
-     * User associated with this Bpay.
-     *
-     * @var \EoneoPay\PhpSdk\Endpoints\User
-     */
-    protected $user;
 
     /**
      * Bpay constructor.
--- tests/Endpoints/PaymentSources/BpayTest.php
+++ tests/Endpoints/PaymentSources/BpayTest.php
@@ -12,58 +12,28 @@ use Tests\EoneoPay\PhpSdk\TestCase;
 class BpayTest extends TestCase
 {
     /**
-     * Test get Bpay token information returns expected information about the Bpay endpoint.
+     * Test get bpay payment source endpoint getters. There is no public api to create a bpay payment source
+     * hence only testing getters.
      *
      * @return void
      */
-    public function testGetBpayTokenInfo(): void
+    public function testBpayPaymentSource(): void
     {
         $hash = $this->generateId();
 
-        $apiManager = $this->createApiManager([
-            'ewallet' => [
-                'id' => $this->generateId(),
-                'pan' => 'D...WERE',
-                'primary' => true,
-                'reference' => 'DY34YCWERE',
-                'type' => 'ewallet',
-                'soft_limit' => null,
-                'credit_limit' => null,
-                'currency' => 'AUD',
-                'user' => [
-                    'email' => 'user@email.test',
-                    'metadata' => [],
-                    'name' => 'John Wick',
-                ],
-            ],
+        $paymentSource = new Bpay([
             'id' => $hash,
             'pan' => 'B...PAY',
             'type' => 'bpay',
             'biller_code' => '112233',
             'biller_name' => 'Test',
             'reference_number' => '1793768381',
-            'user' => [
-                'email' => 'user@email.test',
-                'metadata' => [],
-                'name' => 'John Wick',
-            ],
         ]);
 
-        /** @var \EoneoPay\PhpSdk\Endpoints\PaymentSource $paymentSource */
-        $paymentSource = $apiManager->find(
-            Bpay::class,
-            (string)\getenv('PAYMENTS_API_KEY'),
-            $hash
-        );
-
-        self::assertInstanceOf(Bpay::class, $paymentSource);
-        /** @var \EoneoPay\PhpSdk\Endpoints\PaymentSources\Bpay $paymentSource */
         self::assertSame($hash, $paymentSource->getId());
         self::assertSame('B...PAY', $paymentSource->getPan());
         self::assertSame('112233', $paymentSource->getBillerCode());
         self::assertSame('Test', $paymentSource->getBillerName());
         self::assertSame('1793768381', $paymentSource->getReferenceNumber());
-        self::assertNull($paymentSource->getAllocationEwallet());
-        self::assertSame('DY34YCWERE', $paymentSource->getEwallet()->getReference());
     }
 }
