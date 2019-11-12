3f5b29294225305643e3e50c9aca9abd7137a6e8
Edward Murrell <edward@loyaltycorp.com.au>
2019-11-12 16:36:01 +1100

    PYMT-1506: Add missing \@Groups anontations to BalanceTrait.

--- src/Traits/BalanceTrait.php
+++ src/Traits/BalanceTrait.php
@@ -3,10 +3,14 @@ declare(strict_types=1);
 
 namespace EoneoPay\PhpSdk\Traits;
 
+use Symfony\Component\Serializer\Annotation\Groups;
+
 trait BalanceTrait
 {
     /**
      * The usable money on this Ewallet.
+     *
+     * @Groups({"get"})
      *
      * @var string|null
      */
@@ -15,12 +19,16 @@ trait BalanceTrait
     /**
      * Current balance of this Ewallet.
      *
+     * @Groups({"get"})
+     *
      * @var string|null
      */
     protected $balance;
 
     /**
      * The credit limit on this Ewallet.
+     *
+     * @Groups({"get"})
      *
      * @var string|null
      */
