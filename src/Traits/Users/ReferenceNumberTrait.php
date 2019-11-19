b1cadce3fe831a4be9e3f751e5418c3b57b7aae3
Edward Murrell <edward@loyaltycorp.com.au>
2019-11-20 08:58:46 +1100

    PYMT-1506: Remove ability to set reference number.:

--- src/Traits/Users/ReferenceNumberTrait.php
+++ src/Traits/Users/ReferenceNumberTrait.php
@@ -8,8 +8,6 @@ use Symfony\Component\Serializer\Annotation\Groups;
 trait ReferenceNumberTrait
 {
     /**
-     * @Groups({"create"})
-     *
      * @var string|null
      */
     protected $referenceNumber;
