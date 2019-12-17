9127f0b839a11d72ba3b566fa8114b3fa6610f02
BoneyM <51302697+bmm83@users.noreply.github.com>
2019-12-18 09:22:59 +1100

    PYMT-1525: Add get contract fees endpoint (#69)
    PYMT-1525: Add get contract fees endpoint #69

--- tests/Endpoints/Users/ContractTest.php
+++ tests/Endpoints/Users/ContractTest.php
@@ -22,6 +22,7 @@ final class ContractTest extends TestCase
     {
         $contract = $this->createApiManager(
             [
+                'action' => 'debit',
                 'created_at' => '2019-02-25T05=>43=>31Z',
                 'currency' => 'AUD',
                 'ewallet' => [
@@ -41,6 +42,7 @@ final class ContractTest extends TestCase
                 ],
                 'fixed_fee' => '0.02',
                 'group' => 'Mastercard',
+                'type' => 'contract',
                 'updated_at' => '2019-02-26T03=>19=>03Z',
                 'user' => [
                     'created_at' => '2019-02-24T23=>34=>11Z',
@@ -68,5 +70,111 @@ final class ContractTest extends TestCase
          * @see https://youtrack.jetbrains.com/issue/WI-37859 - typehint required until PhpStorm recognises assertion
          */
         self::assertInstanceOf(Ewallet::class, $contract->getEwallet());
+    }
+
+    /**
+     * Test Contract fees getters.
+     *
+     * @return void
+     */
+    public function testGetters(): void
+    {
+        $user = new User(['id' => 'external-user-id']);
+        $ewallet = new Ewallet([]);
+        $contract = new Contract([
+            'action' => 'debit',
+            'currency' => 'AUD',
+            'ewallet' => $ewallet,
+            'group' => 'mastercard',
+            'fixed_fee' => '0.02',
+            'type' => 'contract',
+            'user' => $user,
+            'variable_rate' => '0.10',
+        ]);
+
+        self::assertSame('debit', $contract->getAction());
+        self::assertSame('mastercard', $contract->getGroup());
+        self::assertSame('AUD', $contract->getCurrency());
+        self::assertSame($ewallet, $contract->getEwallet());
+        self::assertSame('0.02', $contract->getFixedFee());
+        self::assertSame('0.10', $contract->getVariableRate());
+        self::assertSame($user, $contract->getUser());
+    }
+
+    /**
+     * Test get endpoint is successful.
+     *
+     * @return void
+     */
+    public function testGetContractFees(): void
+    {
+        $apiManager = $this->createApiManager(
+            [
+                'action' => 'debit',
+                'created_at' => '2019-02-25T05:43:31Z',
+                'currency' => 'AUD',
+                'ewallet' => [
+                    'created_at' => '2019-02-24T23:34:11Z',
+                    'currency' => 'AUD',
+                    'id' => '6e967a8e9971aab24d2db4e932ca1a06',
+                    'pan' => 'Y...K8Y7',
+                    'primary' => true,
+                    'reference' => 'YNGANFK8Y7',
+                    'type' => 'ewallet',
+                    'updated_at' => '2019-02-24T23:34:11Z',
+                    'user' => [
+                        'created_at' => '2019-02-24T23:34:11Z',
+                        'email' => 'examples@user.test',
+                        'id' => '3T93F7TXCVGX4ZV7AFW2',
+                        'updated_at' => '2019-02-24T23:34:11Z',
+                    ],
+                ],
+                'fixed_fee' => '0.02',
+                'group' => 'Mastercard',
+                'type' => 'contract',
+                'updated_at' => '2019-02-26T03:19:03Z',
+                'user' => [
+                    'created_at' => '2019-02-24T23:34:11Z',
+                    'email' => 'examples@user.test',
+                    'id' => '3T93F7TXCVGX4ZV7AFW2',
+                    'updated_at' => '2019-02-24T23:34:11Z',
+                ],
+                'variable_rate' => '0.10',
+            ],
+            201
+        );
+
+        /** @var \EoneoPay\PhpSdk\Endpoints\Users\Contract $contract */
+        $contract = $apiManager->findOneBy(
+            Contract::class,
+            (string)\getenv('PAYMENTS_API_KEY'),
+            ['userId' => '3T93F7TXCVGX4ZV7AFW2']
+        );
+
+        self::assertSame('0.10', $contract->getVariableRate());
+        self::assertSame('Mastercard', $contract->getGroup());
+        self::assertSame('AUD', $contract->getCurrency());
+        self::assertSame('0.02', $contract->getFixedFee());
+        self::assertSame('debit', $contract->getAction());
+        self::assertInstanceOf(User::class, $contract->getUser());
+        self::assertInstanceOf(Ewallet::class, $contract->getEwallet());
+    }
+
+    /**
+     * Ensure uris have correct endpoints.
+     *
+     * @return void
+     */
+    public function testUris(): void
+    {
+        $user = new User([]);
+        $contract = new Contract([
+            'user' => $user
+        ]);
+
+        $createUri = \sprintf('/users/%s/contracts', $user->getId());
+        $getUri = \sprintf('/users/%s/contracts', $user->getId());
+
+        self::assertSame(['create' => $createUri, 'get' => $getUri], $contract->uris());
     }
 }
