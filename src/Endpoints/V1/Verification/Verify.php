95dd2c2ff5b4c216612a174d0180200b6d3a2e7c
Rashmit Singh <rashmit@loyaltycorp.com.au>
2020-03-30 09:54:50 +1100

    verification v1 objects

---
+++ src/Endpoints/V1/Verification/Verify.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace EoneoPay\PhpSdk\Endpoints\V1\Verification;
+
+use EoneoPay\PhpSdk\Endpoints\Verification\Verify as BaseVerify;
+
+class Verify extends BaseVerify
+{
+}
