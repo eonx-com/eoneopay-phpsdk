8ecb17c6f14d6f62259cfb1c5593fa33d2c098c1
Rashmit Singh <rashmit@loyaltycorp.com.au>
2020-03-30 08:53:28 +1100

    example for moving objects to v1

--- src/Endpoints/Amount.php
+++ src/Endpoints/Amount.php
@@ -3,25 +3,16 @@ declare(strict_types=1);
 
 namespace EoneoPay\PhpSdk\Endpoints;
 
-use EoneoPay\PhpSdk\Traits\AmountTrait;
-use LoyaltyCorp\SdkBlueprint\Sdk\Entity;
+use EoneoPay\PhpSdk\Endpoints\V1\Amount as V1Amount;
 
 /**
  * @method string|null getCurrency()
  * @method string|null getPaymentFee()
  * @method string|null getSubtotal()
  * @method string|null getTotal()
+ *
+ * @deprecated Use V1 Amount object.
  */
-class Amount extends Entity
+class Amount extends V1Amount
 {
-    use AmountTrait;
-
-    /**
-     * {@inheritdoc}
-     */
-    public function uris(): array
-    {
-        // There are no actions directly on amounts
-        return [];
-    }
 }
