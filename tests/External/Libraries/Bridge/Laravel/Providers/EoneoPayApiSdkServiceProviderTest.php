4ae4811076fd928401f22eb49ddc6bbc511a91c8
Rashmit Singh <rashmit@loyaltycorp.com.au>
2020-04-01 10:39:36 +1100

    standards fix v6

--- tests/External/Libraries/Bridge/Laravel/Providers/EoneoPayApiSdkServiceProviderTest.php
+++ tests/External/Libraries/Bridge/Laravel/Providers/EoneoPayApiSdkServiceProviderTest.php
@@ -33,7 +33,6 @@ final class EoneoPayApiSdkServiceProviderTest extends TestCase
             EoneoPayApiManager::class,
             $this->app->make(EoneoPayApiManagerInterface::class)
         );
-
     }
 
     /**
